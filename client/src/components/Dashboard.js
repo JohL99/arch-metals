d93374a122ff2af7b0f3258095a0fee22194fbcb
chris <clamprec@gmail.com>
2020-09-12 15:01:41 +0200

    12Sep5

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -14,18 +14,18 @@ class Dashboard extends PureComponent {
       </div>
     }
     chat(){
-        (function(d,t) {
-            var BASE_URL = "https://app.chatwoot.com";
-            var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src= BASE_URL + "/packs/js/sdk.js";
-            s.parentNode.insertBefore(g,s);
-            g.onload=function(){
-              window.chatwootSDK.run({
-                websiteToken: 's6p8vqywRP8ouGyMuSpBQdSC',
-                baseUrl: BASE_URL
-              })
-            }
-          })(document,"script");
+      (function(d,t) {
+        var BASE_URL = "https://app.chatwoot.com";
+        var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src= BASE_URL + "/packs/js/sdk.js";
+        s.parentNode.insertBefore(g,s);
+        g.onload=function(){
+          window.chatwootSDK.run({
+            websiteToken: 's6p8vqywRP8ouGyMuSpBQdSC',
+            baseUrl: BASE_URL
+          })
+        }
+      })(document,"script");
     }
 }
 export default Dashboard
