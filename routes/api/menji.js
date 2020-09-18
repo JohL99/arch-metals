1d98e190c8be8a44ea0c68f036b6c80009f7d5c5
chris <clamprec@gmail.com>
2020-09-18 11:11:31 +0200

    18Sep3

--- routes/api/menji.js
+++ routes/api/menji.js
@@ -396,7 +396,13 @@ router.get("/userd/:lemois&:commodity&:user", (req, res) => {
       { commodity: req.params.commodity },
       { user: req.params.user },
     ],
+  
+        $group: { _id: { user: "$user", mois: "$mois", },
+        lastEntry: { $last: "$_dateforecast" },
+        detail: { $last: "$$ROOT" }, },
+      
   })
+
     .sort({ dateforecast: -1, commodity: 1, mois: 1, user: 1 })
     .then((menjis) => {
       res.json(menjis);
